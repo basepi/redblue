3f09aec4ca10516da1753850556ac8d275e2587e
Colton Myers <colton.myers@gmail.com>
2012-12-01 11:07:56 -0700

    Update to parse converge % and converge tiles (t)

--- redblue_omp.cpp
+++ redblue_omp.cpp
@@ -20,16 +20,22 @@ using namespace std;
 int main (int argc, char** argv) {
 
     int rows, cols;
+    double conv;
+    double tiles;
     int** grid1;
     int** grid2;
+    timeval start, end;
+    double elapsedtime;
 
-    if(argc < 4) {
-        cout<<"Usage:  redblue_omp <rows> <cols> <filename>"<<endl;
+    if(argc < 5) {
+        cout<<"Usage:  redblue_omp <rows> <cols> <filename> <converge_val> <converge_tiles>"<<endl;
         return 0;
     }
 
     rows = atoi(argv[1]);
     cols = atoi(argv[2]);
+    conv = atof(argv[4]);
+    tiles = atof(argv[5]);
 
     grid1 = new int[rows][cols];
     grid2 = new int[rows][cols];
@@ -47,4 +53,19 @@ int main (int argc, char** argv) {
     }
 
     gridfile.close();
+
+
+    /* Actual computation */
+
+    gettimeofday(&start, NULL);
+
+
+    gettimeofday(&end, NULL);
+
+    /* End of computation */
+
+    elapsedtime = (end.tv_sec - start.tv_sec) * 1000.0;
+    elapsedtime += (end.tv_usec - start.tv_usec) / 1000.0;
+    cout<<"Time: "<<elapsedtime<<" ms."<<endl<<endl;
+
 }
